91fe359f367db3e1af8ea3d22976f4ccde4b20c7
gsr <gshawnr@gmail.com>
2022-09-05 11:11:35 -0700

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright &copy; {year}</p>
+    </footer>
+  );
+};
+
+export default Footer;
